164c1786f380345667ce06b366a8dfdaa6ee86fc
skrzero <Formationafec@outlook.fr>
2025-05-06 16:38:23 +0200

    menu-js

---
+++ assets/scripts/data/menu.js
@@ -0,0 +1,23 @@
+export const links = [
+    {
+        title: 'Accueil',
+        url: "index.html",
+    },
+    {
+        title: "Meteo",
+        url: 'meteo.html',
+        icone: ""
+    },
+    {
+        title: 'Minuteur',
+        url: "",
+    },
+    {
+        title: 'Calendrier',
+        url: "",
+    },
+    {
+        title: 'Mails',
+        url: "",
+    },
+ ];     
--- dist/js/theme.js
+++ dist/js/theme.js
@@ -1,1 +1,117 @@
-(()=>{let e=document.querySelector(".header_btn_Menu"),n=document.getElementById("main-menu"),t=document.querySelector(".close-mainMenu");e&&n&&e.addEventListener("click",(function(){n.classList.add("visible")})),t&&n&&t.addEventListener("click",(function(){n.classList.remove("visible")}))})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+/******/ 	var __webpack_modules__ = ({
+
+/***/ "./assets/scripts/base.js":
+/*!********************************!*\
+  !*** ./assets/scripts/base.js ***!
+  \********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _components_Menu_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./components/Menu.js */ \"./assets/scripts/components/Menu.js\");\n\nwindow.addEventListener(\"DOMContentLoaded\", () => {\n  const MainMenu = new _components_Menu_js__WEBPACK_IMPORTED_MODULE_0__.Menu();\n  MainMenu.init();\n});\nlet btnMenu = document.querySelector(\".header_btn_Menu\"), mainMenu = document.getElementById(\"main-menu\"), btnCloseMenu = document.querySelector(\".close-mainMenu\");\nif (btnMenu && mainMenu) {\n  btnMenu.addEventListener(\"click\", function() {\n    mainMenu.classList.add(\"visible\");\n  });\n}\nif (btnCloseMenu && mainMenu) {\n  btnCloseMenu.addEventListener(\"click\", function() {\n    mainMenu.classList.remove(\"visible\");\n  });\n}\n\n\n//# sourceURL=webpack://Afec_starter_kit/./assets/scripts/base.js?");
+
+/***/ }),
+
+/***/ "./assets/scripts/components/Menu.js":
+/*!*******************************************!*\
+  !*** ./assets/scripts/components/Menu.js ***!
+  \*******************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   Menu: () => (/* binding */ Menu)\n/* harmony export */ });\n/* harmony import */ var _data_menu__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ../data/menu */ \"./assets/scripts/data/menu.js\");\n\nclass Menu {\n  constructor() {\n    this.menuContainer = document.querySelector(\"#menu-js\");\n  }\n  init() {\n    let menuTemplate = `<ul>`;\n    _data_menu__WEBPACK_IMPORTED_MODULE_0__.links.forEach((link) => {\n      menuTemplate += `<li><a href=\"\" icone=\"\" title=\"\">${link.title}</a></li>`;\n    });\n    menuTemplate += `</ul>`;\n    this.render(menuTemplate);\n  }\n  render(content) {\n    this.menuContainer.innerHTML = content;\n  }\n}\n\n\n//# sourceURL=webpack://Afec_starter_kit/./assets/scripts/components/Menu.js?");
+
+/***/ }),
+
+/***/ "./assets/scripts/data/menu.js":
+/*!*************************************!*\
+  !*** ./assets/scripts/data/menu.js ***!
+  \*************************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   links: () => (/* binding */ links)\n/* harmony export */ });\nconst links = [\n  {\n    title: \"Accueil\",\n    url: \"index.html\"\n  },\n  {\n    title: \"Meteo\",\n    url: \"meteo.html\",\n    icone: \"\"\n  },\n  {\n    title: \"Minuteur\",\n    url: \"\"\n  },\n  {\n    title: \"Calendrier\",\n    url: \"\"\n  },\n  {\n    title: \"Mails\",\n    url: \"\"\n  }\n];\n\n\n//# sourceURL=webpack://Afec_starter_kit/./assets/scripts/data/menu.js?");
+
+/***/ }),
+
+/***/ "./assets/styles/base.scss":
+/*!*********************************!*\
+  !*** ./assets/styles/base.scss ***!
+  \*********************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n// extracted by mini-css-extract-plugin\n\n\n//# sourceURL=webpack://Afec_starter_kit/./assets/styles/base.scss?");
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __webpack_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__webpack_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__webpack_require__.o(definition, key) && !__webpack_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__webpack_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__webpack_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/************************************************************************/
+/******/ 	
+/******/ 	// startup
+/******/ 	// Load entry module and return exports
+/******/ 	// This entry module can't be inlined because the eval devtool is used.
+/******/ 	__webpack_require__("./assets/scripts/base.js");
+/******/ 	var __webpack_exports__ = __webpack_require__("./assets/styles/base.scss");
+/******/ 	
+/******/ })()
+;
