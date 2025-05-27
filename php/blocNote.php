3b4a0f455b95440dfa16719690cbf90698240439
skrzero <Formationafec@outlook.fr>
2025-05-27 16:31:08 +0200

    blocnote

--- php/blocNote.php
+++ php/blocNote.php
@@ -3,10 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4Q6Gf2aSP4eDXB8Miphtr37CMZZQ5oXLH2yaXMJ2w8e2ZtHTl7GptT4jmndRuHDT" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.6/dist/js/bootstrap.bundle.min.js" integrity="sha384-j1CDi7MgGQ12Z7Qab0qlWQ/Qqz24Gc6BM0thvEMVjHnfYGF0rmFCozFSxQBxwHKO" crossorigin="anonymous"></script>
     <title>blocNote</title>
 </head>
 <body>
     <h1>bloc notes</h1>
-    
+<div class="container">
+    <div class="row">
+        <div class="col"></div>
+    </div>
+</div>    
 </body>
 </html>
